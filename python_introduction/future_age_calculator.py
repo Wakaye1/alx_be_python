14154613e688d365af0476e11fea84acce944e27
Wakaye1 <wakayeabba1@gmail.com>
2024-07-01 04:01:15 +0800

    added quite a number

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,3 @@
+age=int(input("How old are you?"))
+Twenty=age+27
+print("In 2050, you will be {} years old.".format(Twenty))
---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,4 @@
+hours = 2
+one_hour=3600
+seconds=one_hour*hours
+print(" {} hour(s) is {} seconds.".format(hours,seconds))
