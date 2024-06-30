13a43e397cd1bead7530156f5df8203eabd44ee8
Wakaye1 <wakayeabba1@gmail.com>
2024-07-01 03:17:13 +0800

    Initial commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1=10
+number2=5
+Addition=number1+number2
+Sub=number1-number2
+Mult=number1*number2
+print("Addition of {} and {} is {}".format(number1,number2,Addition))
+print("Subtraction of {} and {} is {}".format(number1,number2,Sub))
+print("Multiplication of {} and {} is {}".format(number1,number2,Mult))
